b2d42282ca516d4464772be5c0739a7e4757e2cb
oladosumoses24@gmail.com <oladosumoses24@gmail.com>
2021-08-11 13:03:59 +0100

    priming things up

---
+++ storage.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  9 10:19:55 2021
+
+@author: Unrated
+"""
+
+<li>
+    <div style="background-color: blue"><h1 class="h2">Music Stream</h1></div>
+</li>
+<li>
+    <div><h1 class="h2">Movie Stream</h1></div>
+</li>
+<li>
+    <div><h1 class="h2">File Transfer</h1></div>
+</li>
+
+    /*let ul = $(".slider ul")
+    let slides = $(".slider li").length
+    let it = 907.667; // the width of the main window
+    $(".forward").click(function(){
+        let margin = Number(ul.css("margin-left").slice(0,-2))-it
+        let mage = (Math.round(margin) >= -it*(slides-1) ? margin : 0)
+        ul.stop(true, true).animate({"margin-left": mage+"px"}, 200);
+        navel[Math.round(-mage/it)].click()
+    })
+    
+    $(".back").click(function(){
+        let margin = Number(ul.css("margin-left").slice(0,-2))+it
+        let mage = (Math.round(margin) > 0 ? -it*(slides-1) : margin) // the round is begin of the possible fractional nature of margin
+        console.log(margin, mage, "backsit")
+        ul.stop(true, true).animate({"margin-left": mage+"px"}, 200); // the condition is to allow wrapping
+        console.log("index", navel.length, Math.round(-mage/it))
+        navel[Math.round(-mage/it)].click()
+    })*/
